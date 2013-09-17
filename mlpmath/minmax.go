40d926454e092bc55c02715e6cc3105e17213631
joshua bowles <bowlesling@gmail.com>
2013-09-17 08:30:38 -0600

    ported the math helper functions package and the string helper functions package

---
+++ mlpmath/minmax.go
@@ -0,0 +1,33 @@
+/*
+* Copyright ©2013 The nlpt Authors. All rights reserved.
+* Use of this source code is governed by a BSD-style
+* license that can be found in the LICENSE file.
+*
+* minmax define functions for determining a Min and/or Max int32
+ */
+package nlpmath
+
+const (
+	MaxInteger = int(^uint(0) >> 1) // max largest int 9223372036854775807
+	MinInteger = (-MaxInteger - 1)
+)
+
+func MinInt(a ...int) int {
+	min := MaxInteger
+	for _, i := range a {
+		if i < min {
+			min = i
+		}
+	}
+	return min
+}
+
+func MaxInt(a ...int) int {
+	max := int(0)
+	for _, i := range a {
+		if i > max {
+			max = i
+		}
+	}
+	return max
+}
---
+++ mlpmath/multinomial.go
@@ -0,0 +1,44 @@
+/*
+* Copyright ©2013 The nlpt Authors. All rights reserved.
+* Use of this source code is governed by a BSD-style
+* license that can be found in the LICENSE file.
+*
+* multinomial provieds the Multinomial Distribution
+ */
+package nlpmath
+
+// Multinomial Counts
+type MultiCount struct {
+	counts map[int]int
+}
+
+// Multinomial Distribution
+type Multinomial struct {
+	distribution map[int]float64
+}
+
+func (mc *MultiCount) Count() {
+	mc.counts = make(map[int]int)
+}
+
+func (mc *MultiCount) Increment(key int) {
+	if _, ok := mc.counts[key]; ok {
+		mc.counts[key] = 0
+	}
+	mc.counts[key]++
+}
+
+func (multi *Multinomial) Probability(key int) float64 {
+	return multi.distribution[key]
+}
+
+func (multi *Multinomial) MaximumLikelihood(mc MultiCount) {
+	multi.distribution = make(map[int]float64)
+	sum := 0.0
+	for _, count := range mc.counts {
+		sum += float64(count)
+	}
+	for key, count := range mc.counts {
+		multi.distribution[key] = float64(count) / sum
+	}
+}
